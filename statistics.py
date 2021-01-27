a94263d6d9a76883059b8b4035be7fbffb1705d3
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-01-27 11:23:28 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,21 +1,13 @@
 
+import math
 def calculateStats(numbers):
   l=len(numbers)
   res={"avg":0,"max":0,"min":0};
   if l!=0:
-    sum=0; max=numbers[0]; min=numbers[0];
-    for i in range(0,l):
-      sum+=numbers[i];
-      if numbers[i]<min:
-        min=numbers[i];
-      if numbers[i]>max:
-        max=numbers[i];
-    avg1=sum/l;
-    res["avg"]=avg1
-    res["max"]=max
-    res["max"]=min
-  
-  
-  #use dictionary for returning the function ; avg, min and max? 
-  #define email and led alert functions with only print statements
+    res["avg"]=(sum(numbers)/l)
+    res["max"]=max(numbers)
+    res["min"]=min(numbers)
+   else:
+    res["avg"]=res["max"]=res["min"]=math.nan;
+    
   return res
