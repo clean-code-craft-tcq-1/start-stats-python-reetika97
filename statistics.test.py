1ec29984805bb8267fc1a841ddbe70608ed52ace
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-01-28 10:58:36 +0530

    Update statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -14,12 +14,12 @@ class StatsAlerter:
     self.ea=alerts[0].emailSent
     self.la=alerts[1].ledGlows    
     
-  def checkAndAlert(lt):
+  def checkAndAlert(self,lt):
     for i in range(0,len(lt)):
         if lt[i]>self.mt:
             self.ea=1
             self.la=1
-    return alerts
+    return [self.ea,self.la]
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
@@ -39,14 +39,14 @@ class StatsTest(unittest.TestCase):
     # Design the assert here.
     # Use nan and isnan in https://docs.python.org/3/library/math.html
 
-  #def test_raise_alerts_when_max_above_threshold(self):
-    #emailAlert = EmailAlert()
-    #ledAlert = LEDAlert()
-    #maxThreshold = 10.5
-    #statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
-    #statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
-    #self.assertTrue(emailAlert.emailSent)
-    #self.assertTrue(ledAlert.ledGlows)
+  def test_raise_alerts_when_max_above_threshold(self):
+    emailAlert = EmailAlert()
+    ledAlert = LEDAlert()
+    maxThreshold = 10.5
+    statsAlerter = StatsAlerter(maxThreshold, [emailAlert, ledAlert])
+    statsAlerter.checkAndAlert([22.6, 12.5, 3.7])
+    self.assertTrue(emailAlert.emailSent)
+    self.assertTrue(ledAlert.ledGlows)
 
 if __name__ == "__main__":
   unittest.main()
