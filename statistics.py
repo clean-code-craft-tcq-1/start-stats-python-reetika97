f27ece985bd834bde990efc292330f862c4d3ec9
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-01-28 11:45:31 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -2,7 +2,7 @@
 import math
 def calculateStats(numbers):
   l=len(numbers)
-  res={"avg":0,"max":0,"min":0};
+  results={"avg":0,"max":0,"min":0};
   if l!=0:
     res["avg"]=(sum(numbers)/l)
     res["max"]=max(numbers)
@@ -10,4 +10,4 @@ def calculateStats(numbers):
   else:
     res["avg"]=res["max"]=res["min"]=math.nan;
     
-  return res
+  return results
